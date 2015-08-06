c976c25973b2b3cb55849bdb443733f97a9b28a6
Greg McMullen <mcmullen.greg@gmail.com>
2015-08-06 19:02:19 -0400

    Added front-page and cleaned up page code

--- page.php
+++ page.php
@@ -9,13 +9,24 @@
 */
 
 get_header(); ?>
-	
-	<div class="main">
-		<?php while ( have_posts() ) : the_post(); ?>
-            <?php get_template_part( 'partials/content', 'page' ); ?>
-            <?php comments_template( '', true ); ?>
-        <?php endwhile; // end of the loop. ?>
-	</div>
+	<main class="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		// End the loop.
+		endwhile;
+		?>
+		
+	</main>
 	
 	<?php get_sidebar(); ?>
 
