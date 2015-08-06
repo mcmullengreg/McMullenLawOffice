16bb2743a6e502c631c9db1b3d8150c4c73e487e
Greg McMullen <mcmullen.greg@gmail.com>
2015-08-06 18:46:14 -0400

    Added content parts

--- partials/content-page.php
+++ partials/content-page.php
@@ -1,18 +1,33 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="entry-header">
-<?php if ( ! is_page_template( 'page-templates/front-page.php' ) ) :
-    the_post_thumbnail();
-endif; ?>
-<h1 class="entry-title"><?php the_title(); ?></h1>
-</header>
+<?php
+/**
+ * The template used for displaying page content
+ */
+?>
 
-<div class="entry-content">
-<?php the_content();
-wp_link_pages( array( 
-    'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 
-    'after' => '</div>' ) ); ?>
-</div><!-- .entry-content -->
-<footer class="entry-meta">
-<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
-</footer><!-- .entry-meta -->
-</article><!-- #post -->
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+		// Post thumbnail.
+		#twentyfifteen_post_thumbnail();
+	?>
+
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'mlo' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'mlo' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php edit_post_link( __( 'Edit', 'mlo' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer><!-- .entry-footer -->' ); ?>
+
+</article><!-- #post-## -->
