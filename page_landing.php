84e79554f9d36e963191f2b6b12299087325f3c8
Greg McMullen <mcmullen.greg@gmail.com>
2015-08-07 00:08:40 -0400

    Basic loop content pages

--- page_landing.php
+++ page_landing.php
@@ -5,12 +5,25 @@ Template Name: Landing Page Template
 
 get_header('landing'); ?>
 	
-	<div class="main landing-page">
-		<?php while ( have_posts() ) : the_post(); ?>
-            <?php get_template_part( 'partials/content', 'page' ); ?>
-            <?php comments_template( '', true ); ?>
-        <?php endwhile; // end of the loop. ?>
-	</div>
+	<main class="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'partials/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		// End the loop.
+		endwhile;
+		?>
+		
+	</main>
+	<? # Need to figure out what form software to use ?>
 	<div class="sidebar landing-page">
 		<div class="widget widget_contact">
 			<h5>
